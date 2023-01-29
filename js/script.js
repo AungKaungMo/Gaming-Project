ce4b8b65ae55ea2cf65881575dfb6686450dac7e
AungKaungMoe <moea74759@gmail.com>
2023-01-29 19:37:04 +0630

    Games Page And search bar active

--- js/script.js
+++ js/script.js
@@ -5,16 +5,150 @@ const testiArray = [
     {customerImage : "../img/cus4.jpg", customerName : "Miyoshi AA", CustomerLocation : "Japan, Tokyo", rating : "5.0", message : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."},
   ];
 $(document).ready(function() {
+
+    fetch("http://localhost:3001/gameDetails")
+    .then(res => res.json())
+    .then(data => {
+      gamesArray = data;
+      let atext = "<a href='../html/shop.html'>";
+      let aclose = "</a>";
+        addingGameProducts(gamesArray, "gridContainer", atext, aclose,'');
+        filterItems("gridContainer", "gameTypeBtn");
+      
+    } );
+  
+    fetch("http://localhost:3001/gameBuyDetails")
+    .then(res => res.json())
+    .then(data => {
+      gameDisplayArray = data
+      getUserClickGame();
+    });
+  
+    fetch("http://localhost:3001/productDetails")
+    .then(res => res.json())
+    .then(data => {
+      sellProductsArray = data;
+      let d =  "data-bs-target='#exampleModalToggle' data-bs-toggle='modal'";
+    addingGameProducts(sellProductsArray, "gridProductContainer",'','',d);
+    filterItems("gridProductContainer", "productTypeBtn");
+    getUserClickGame();
+    });
+  
+
     $("#slideBtn").click(function () {
         $(".resBar").addClass("resBarAdd");
       });
       $("#close").click(function () {
         $(".resBar").removeClass("resBarAdd");
       });
-      
+
+      let concatClassName, concatClassVideo;
+      let vidArr = ["firstVd", "secondVd", "thirdVd"];
+      $(".vdPlayBtn").click(function (e) {
+        for (let i = 0; i < vidArr.length; i++) {
+            $($(`.${vidArr[i]}`))[0].pause();
+          document
+            .getElementsByClassName("videoContainer")
+            [i].classList.remove("videoContainerOpen");
+        }
+    
+        concatClassName = e.target.id + "VideoContainer";
+        concatClassVideo = e.target.id + "Vd";
+        $(`.${concatClassName}`).addClass("videoContainerOpen");
+        $(`.${concatClassVideo}`)[0].play();
+      });
+      $(".videoClose").click(function () {
+        $(`.${concatClassName}`).removeClass("videoContainerOpen");
+        $(`.${concatClassVideo}`)[0].pause();
+      });
+
     preparingCustomerReviews();
+
 })
 
+
+function filterItems(con, typeBtn) {
+    var $grid = $(`.${con}`).isotope({
+      // options
+    });
+    // filter items on button click
+    $(`.${typeBtn}`).on("click", "button", function () {
+      var filterValue = $(this).attr("data-filter");
+      reset();
+      $(this).addClass("currentBtn");
+      $grid.isotope({ filter: filterValue });
+    });
+  
+    var filterBtn = $(`.${typeBtn}`).find("button");
+  
+    function reset() {
+      filterBtn.each(function () {
+        $(this).removeClass("currentBtn");
+      });
+    }
+  }
+  
+  function addingGameProducts(arr, container, aOne, aTwo, data) {
+    for (let i = 0; i < arr.length; i++) {
+      $(`.${container}`).append(
+        ` <div class="gameContainerBox ${arr[i].gameType}">
+           <div class="gameImage">
+               <img src="${arr[i].gameImage}">
+           </div>
+           <div class="gameTitle mt-3 mb-4">
+               <div class="h4 mb-0">${arr[i].gameName}</div>
+               <i class="fa-solid fa-fire"></i>
+               <span>${arr[i].popular}</span>
+           </div>
+           <div class="p gameDetails">
+               ${arr[i].gameDetails}
+           </div>
+           <div class="priceAndBuy mt-3">
+               <div class="p fw-bold">${arr[i].price}</div>
+               <div class="cart">
+               ${aOne}
+                   <i class="fa-solid fa-cart-shopping buyGame" ${data} buy-game-data="${arr[i].gameName}"></i>
+                  ${aTwo}
+               </div>
+           </div>`
+      );
+  
+    }
+  }
+  
+  function getUserClickGame() {
+    const buyGame = $(".buyGame");
+      buyGame.click(function() {
+        let dataGameName = $(this).attr('buy-game-data');
+  
+        for(let i =0; i< gameDisplayArray.length; i++){
+          if(dataGameName == gameDisplayArray[i].name){
+  
+             localStorage.setItem('img', gameDisplayArray[i].img);
+             localStorage.setItem('img1', gameDisplayArray[i].img1);
+             localStorage.setItem('img2', gameDisplayArray[i].img2);
+             localStorage.setItem('img3', gameDisplayArray[i].img3);
+             localStorage.setItem('name', gameDisplayArray[i].name);
+             localStorage.setItem('price', gameDisplayArray[i].price);
+             return;
+          }
+        }
+  
+        for(let i=0; i< sellProductsArray.length; i++){
+          if(dataGameName == sellProductsArray[i].gameName){
+           
+            $(".buyImg").attr("src",`${sellProductsArray[i].gameImage}`);
+            $(".gameNaming").text(`${sellProductsArray[i].gameName}`);
+            $(".payPrice").text(`${sellProductsArray[i].price}`);
+           
+            return;
+          }
+        }
+       
+      })
+  
+  }
+  
 window.addEventListener("load",()=> {
 
     let changeJsonCusArray = JSON.parse(localStorage.getItem('customersArray'));
