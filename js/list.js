8e4798159d09beb113c55f80da44f2543e05d2f9
AungKaungMoe <moea74759@gmail.com>
2023-02-04 16:20:08 +0630

    Fixing list's Bug and adding count numbers

--- js/list.js
+++ js/list.js
@@ -1,99 +1,120 @@
 let localArrayForFav = [];
 let localObjForFav = {};
+
 let localArrayForBought = [];
 let localObjForBought = {};
+
 $(document).ready(function () {
   $(".addingFavList").click(function () {
-    addingDivToContainer("listBox", localObjForFav, localArrayForFav);
-    localStorage.setItem("localArrayForFav", JSON.stringify(localArrayForFav));
-    deleteGameOrProduct(localArrayForFav, "localArrayForFav");
+    storage(localArrayForFav, localObjForFav, "listBox", "favListCount");
+    localStorage.setItem("favarray", JSON.stringify(localArrayForFav));
+    deleteItems(localArrayForFav, "favarray", "favListCount");
   });
-  $(".addingProductList").click(function () {
-    addingDivToContainer("listBox2", localObjForBought, localArrayForBought);
-    localStorage.setItem(
-      "localArrayForBought",
-      JSON.stringify(localArrayForBought)
+  //function htl yw apyin mr yw deleteItems() ko khw htr tae atwat 2 khr aloke loke ml
+  //dr so array ka 2 khan pyt twr late ml
+  deleteItems(localArrayForFav, "favarray", "favListCount");
+
+  $(".addingBoughtList").click(function () {
+    storage(
+      localArrayForBought,
+      localObjForBought,
+      "boughtBox",
+      "shopListCount"
     );
-    deleteGameOrProduct(localArrayForBought, "localArrayForBought");
+    localStorage.setItem("boughtArray", JSON.stringify(localArrayForBought));
+    deleteItems(localArrayForBought, "boughtArray", "shopListCount");
   });
-  deleteGameOrProduct(localArrayForFav, "localArrayForFav");
-  deleteGameOrProduct(localArrayForBought, "localArrayForBought");
+  deleteItems(localArrayForBought, "boughtArray", "shopListCount");
 });
-function addingDivToContainer(appendListBox, addObj, arr) {
-  addObj.localName = localStorage.getItem("name");
-
-  const PreventingForNextClick = arr.find((element) => {
-    //if name is the same it is the already in favourite
-    if (element.localName == addObj.localName) {
-      return true;
-    }
-  });
-  if (PreventingForNextClick) {
-    return;
-  }
-  addObj.add = true;
-  addObj.localId = arr.length;
-  addObj.localImage = localStorage.getItem("img");
-  addObj.localPrice = localStorage.getItem("price");
-
-  addingList(
-    appendListBox,
-    localStorage.getItem("img"),
-    localStorage.getItem("name"),
-    localStorage.getItem("price"),
-    arr.length
-  );
-
-  arr.push(addObj);
-  console.log(addObj);
-  console.log(arr);
-}
-function deleteGameOrProduct(arr, arrayName) {
-  $(".bin").click(function () {
-    console.log("click");
-    let clickTargetId = $(this).parent().parent().parent().attr("id");
-
-    for (let i = 0; i < arr.length; i++) {
-      if (clickTargetId == arr[i].localId) {
-        arr.splice(i, 1);
-      }
-    }
-    $(this).parent().parent().parent().remove();
-    localStorage.setItem(`${arrayName}`, JSON.stringify(arr));
-  });
-}
 
 window.addEventListener("load", () => {
-  loadingPrepare("listBox", "localArrayForFav", localArrayForFav);
-  loadingPrepare("listBox2", "localArrayForBought", localArrayForBought);
-});
-function loadingPrepare(appendListBox, arrayName, arr) {
-  let localArrayParse = JSON.parse(localStorage.getItem(`${arrayName}`));
-  if (localArrayParse == null) {
+  //loading localfavarray
+  let parseArray = JSON.parse(localStorage.getItem("favarray"));
+  if (parseArray == null) {
     return;
   }
-  arr = localArrayParse;
-  for (let i = 0; i < localArrayParse.length; i++) {
-    addingList(
-      appendListBox,
-      localArrayParse[i].localImage,
-      localArrayParse[i].localName,
-      localArrayParse[i].localPrice,
-      localArrayParse[i].localId
+  localArrayForFav = parseArray;
+  for (let i = 0; i < localArrayForFav.length; i++) {
+    addList(
+      "listBox",
+      localArrayForFav[i].localId,
+      localArrayForFav[i].localImage,
+      localArrayForFav[i].localName,
+      localArrayForFav[i].localPrice
     );
   }
+  $(".favListCount").text(localArrayForFav.length);
+
+  // loading localBoughtArray
+  let parseArray2 = JSON.parse(localStorage.getItem("boughtArray"));
+  if (parseArray2 == null) {
+    return;
+  }
+  localArrayForBought = parseArray2;
+  for (let i = 0; i < localArrayForBought.length; i++) {
+    addList(
+      "boughtBox",
+      localArrayForBought[i].localId,
+      localArrayForBought[i].localImage,
+      localArrayForBought[i].localName,
+      localArrayForBought[i].localPrice
+    );
+  }
+  $(".shopListCount").text(localArrayForBought.length);
+});
+
+function storage(array, arrayObject, appendingListBox, count) {
+  //Preventing for next click
+  if (array.length > 0) {
+    for (let i = 0; i < array.length; i++) {
+      if (array[i].localName == localStorage.getItem("name")) {
+        return;
+      }
+    }
+  }
+
+  arrayObject.localName = localStorage.getItem("name");
+  arrayObject.localImage = localStorage.getItem("img");
+  arrayObject.localPrice = localStorage.getItem("price");
+  arrayObject.localId = array.length + 1;
+  array.push(arrayObject);
+  $(`.${count}`).text(array.length);
+  addList(
+    appendingListBox,
+    array.length,
+    localStorage.getItem("img"),
+    localStorage.getItem("name"),
+    localStorage.getItem("price")
+  );
 }
-function addingList(appendBox, img, nam, pri, count) {
-  $(`.${appendBox}`).append(
-    `<div class="favBox mt-3 text-light" id="${count}">
+
+function deleteItems(arrayName, localStorageArrayName, count) {
+  $(".bin").click(function () {
+    let clickTargetId = $(this).parent().parent().parent().attr("id");
+    for (let i = 0; i < arrayName.length; i++) {
+      if (clickTargetId == arrayName[i].localId) {
+        arrayName.splice(i, 1);
+        $(this).parent().parent().parent().remove();
+        localStorage.setItem(
+          `${localStorageArrayName}`,
+          JSON.stringify(arrayName)
+        );
+        $(`.${count}`).text(arrayName.length);
+      }
+    }
+  });
+}
+function addList(appendingListBox, length, image, name, price) {
+  $(`.${appendingListBox}`).append(
+    `<div class="favBox mt-3 text-light" id="${length}">
         <div class="favBoxContainer d-flex">
           <div class="image ms-2 ">
-            <img src="${img}" width="100%" height="100%" style= "object-fit: cover;">
+            <img src="${image}" width="100%" height="100%" style= "object-fit: cover;">
           </div>
           <div class="textContainer ms-3">
-            <h5>${nam}</h5>
+            <h5>${name}</h5>
             <p class="m-0">One of the best ranking game in 1023.</p>
-            <p class="fw-bold mt-1">${pri}</p>
+            <p class="fw-bold mt-1">${price}</p>
           </div>
           <div class="trashIcon ms-4 mt-4">
             <i class="fa-solid fa-trash bin"></i>
